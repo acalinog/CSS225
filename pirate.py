b6efc6779973c935051e7124d7fb20f4af2fa367
acalinog <87920723+acalinog@users.noreply.github.com>
2021-07-24 21:48:53 -0500

    Add files via upload
    MODULE3-Calinog
    pirate.py

---
+++ pirate.py
@@ -0,0 +1,5 @@
+greeting = input("Hello, possible pirate! What's the password?")
+if greeting in ("Arrr!"):
+	print("Go away, pirate.")
+elif greeting in ("What?"):
+    print("Greetings, hater of pirates!")
