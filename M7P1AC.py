73745830251a500805dbb90a062e94ee8a933838
acalinog <87920723+acalinog@users.noreply.github.com>
2021-08-25 09:22:24 -0500

    Add files via upload

---
+++ M7P1AC.py
@@ -0,0 +1,20 @@
+#Abby Calinog
+#August 20, 2021
+#problem 1: Write a function that takes two inputs from a user and 
+#prints whether they are equal or not
+
+def evaluateInput():
+    input1 = input("1:")
+    input2 = input("2:")
+    
+    if input1 == input2:
+        prnit("These values are equal")
+    else:
+        print("These values are not equal")
+        
+    evaluateInput1()
+
+
+
+
+    
---
+++ M7P2AC.py
@@ -0,0 +1,9 @@
+def sum(a, b):
+    sum = a + b
+    a = 1
+    b= 5
+    if sum < 10:
+        print("less than 10")
+    if sum > 10:
+        print("greater than 10")
+    
